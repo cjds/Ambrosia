066d32678dab0ac87da2d03fe379ad622feb9ff3
cjds <cjds@live.com>
2015-04-08 22:25:26 -0400

    test

---
+++ app/src/main/java/me/cjds/ambrosia/NotificationService.java
@@ -0,0 +1,16 @@
+package me.cjds.ambrosia;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NotificationService extends Service {
+    public NotificationService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
